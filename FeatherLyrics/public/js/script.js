dc9760c0e2d9425b37c71103c24e562a27984ec7
OlEkSaNdR0812 <yurchenko.20031208@gmail.com>
2023-04-03 16:18:02 +0300

    Autorize

--- FeatherLyrics/public/js/script.js
+++ FeatherLyrics/public/js/script.js
@@ -1,3 +1,15 @@
+function authorize(){ 
+const CLIENT_ID = '0749df1023d34fe49088b64eae364f85';
+const REDIRECT_URI = 'http://localhost:3000/callback'; // замініть на свій власний
+
+// Перенаправляє користувача на сторінку дозволу на доступ
+const requestAuthorization = () => {
+  const scope = 'user-read-private user-read-email'; // змініть обсяг доступу на свій власний
+  const url = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}&scope=${encodeURIComponent(scope)}`;
+  window.location = url;
+};
+
+}
 //Bridge
 var Gestures = function (output) {
     this.output = output;
