485cf59af608bd5bda6ddb617c0e86b99e3aa780
OlEkSaNdR0812 <yurchenko.20031208@gmail.com>
2023-04-04 17:12:27 +0300

    a

--- FeatherLyrics/src/app.module.ts
+++ FeatherLyrics/src/app.module.ts
@@ -6,15 +6,13 @@ import { VocabularyModule } from './vocabulary/vocabulary.module';
 import { LeasonsModule } from './leasons/leasons.module';
 
 @Module({
-<<<<<<< HEAD
+
   imports: [
     SongsModule,
     VocabularyModule,
     LeasonsModule,
   ],
-=======
-  imports: [SongsModule, VocabularyModule, LeasonsModule],
->>>>>>> 25f0e3915f54cbeb1ff8b838c1d8e7ae0aad4664
+
   controllers: [AppController],
   providers: [AppService],
 })
